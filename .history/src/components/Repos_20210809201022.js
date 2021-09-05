2fbf13eed1df71adad49b92bf4720e45c14c1fb0
Max1997 <max97h@gmail.com>
2021-09-06 00:27:15 +0300

    first commit

--- .history/src/components/Repos_20210809201022.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Repos = ({repos}) => (
-    <React.Fragment>
-        {repos.map(repo => (
-            <div className="card mb-3" key={repo.key}></div>
-        ))}
-    </React.Fragment>
-);
